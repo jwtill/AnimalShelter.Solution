7c369a27534d28269d8b8aa315c522268156e3e0
Josh Tillinghast <jwtill@icloud.com>
2022-04-01 12:58:45 -0700

    WIP trouble shoot adding swaggerDoc info, break for lunch, travel time

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace AnimalShelter
 {
@@ -23,8 +27,30 @@ namespace AnimalShelter
       services.AddDbContext<AnimalShelterContext>(opt =>
           opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
-      services.AddSwaggerGen();
+      services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "A simple example ASP.NET Core Web API",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Shayne Boyer",
+            Email = string.Empty,
+            Url = new Uri("https://twitter.com/spboyer"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Use under LICX",
+            Url = new Uri("https://example.com/license"),
+        }
+    });
+});
+
     }
+
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
