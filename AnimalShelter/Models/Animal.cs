cb2e4f427ee22cd1025067a7b474779177dc90eb
Josh Tillinghast <jwtill@icloud.com>
2022-04-01 10:32:39 -0700

    Add validations

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
+        [Required]
+        [StringLength(23)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(10)]
         public string Species { get; set; }
         public int Age { get; set; }
         public string Sex { get; set; }
+        [Required]
         public bool Adopted { get; set; }
     }
 }
